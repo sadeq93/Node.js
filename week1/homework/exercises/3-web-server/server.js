54c595864149aee037d1eafbfe13f44d2660abee
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2020-10-04 11:08:14 +0200

    debug ex3

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -1,30 +1,27 @@
-/**
- * Exercise 3: Create an HTTP web server
- */
-
-var http = require('http');
-const path = require('path')
-const fs = require('fs')
-// 
-const filePath = path.join(__dirname,'index.html',)
-// 
+const http = require('http');
+const fs   = require('fs'); 
 //create a server
-let server = http.createServer(function (req, res) {
+const server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-  if(req.url === '/')
-  fs.readFile(filePath,(err,content)=>{
-	res.writeHead(200,{'content-type':'text/html'})
-	res.write(content); // Sends a response back to the client
-
-  })
-  if(req.url === '/index.js')
-  fs.readFile(path.join(__dirname,'/index.js',),(err)=>{
-  res.writeHead(200)
-  res.writeHead(200,{'Content-Type': 'application/json'})
+  if          (req.url === '/'){
+  const data =  fs  .readFileSync ('index.html');
+                res .writeHead    (200, {'Content-Type': 'text/html'});
+                res .write        (data); // Sends a response back to the client
+  } else if   (req.url === '/index.js'){
+  const data =  fs  .readFileSync ('index.js');
+                res .writeHead    (200, {'Content-Type': 'application/json'});
+                res .write        (data);
+  }else if    (req.url === '/style.css'){
+  const data =  fs  .readFileSync ('style.css');
+                res .writeHead    (200, {'Content-Type': 'text/css'});
+                res .write        (data);
+  }else {
+	  res.writeHead (404, {'Content-Type': 'text/plain'});
+	  res.write     ('NOT FOUND !!');
+  }
 	res.end(); // Ends the response
-  })
-})
+});
+
+server.listen(3000); // The server starts to listen on port 3000
 
 
-
-server.listen(3300); // The server starts to listen on port 3000
